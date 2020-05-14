0281b520b357c47e829e497a808bf155b174cfc5
Ilya Baryko <38408304+shineRR@users.noreply.github.com>
2020-05-15 01:25:22 +0300

    Update menu.php

--- src/menu.php
+++ src/menu.php
@@ -5,7 +5,7 @@ setStyles();
 
 $page = basename($_SERVER['PHP_SELF']);
 $headerMenu = array('index.php' => 'Home', 'profile.php' => 'Profile', 'serials.php' => 'Serials', 'films.php' => 'Top Films',
-	'extension.php' => 'Extension', 'loginLabPage.php' => 'Log In', 'dbpage.php' => 'DB Lab', 'feedback.php' => 'Feedback', 'pagestyles.php' => 'Set styles');
+	'loginLabPage.php' => 'Log In', 'dbpage.php' => 'DB Lab', 'feedback.php' => 'Feedback', 'pagestyles.php' => 'Set styles');
 
 foreach ($headerMenu as $key => $item) {
 	echo $key == $page ? '<a id="php-button" href="#">' . '<span>' . $item . '</span>' . '</a>' : '<a href="/' . $key . '">' . '<span>' . $item . '</span>' . '</a>';
